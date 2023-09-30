195c1100f4c77f3f49f7bc59298d8707ee7d0d26
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-09-30 14:46:13 -0500

    added logo to hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
