b9e2d6bcca206ce87fc573fc9cb884cee70aa0f3
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-01 15:35:50 -0500

    added styling effect to hero intro

--- app/hero/page.js
+++ app/hero/page.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <>
       <div className="flex height-fix">
         <p className="text-4xl text-ltGray text-center">
-          Hello, I&apos;m <span className="text-yellow">Joseph Stanion.</span>
+          Hello, I&apos;m <span className="text-yellow dark:drop-shadow-[0_0_0.3rem_#ffffff70] animate-pulse">Joseph Stanion.</span>
           <br />
           I&apos;m a full-stack web developer.
         </p>
