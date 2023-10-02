4c9cf34d537c47241c7b8237b0a42c616025fc38
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-02 12:16:30 -0500

    moved hero file content to home page/layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'JS | Joseph Stanion',
-  description: 'Empowering Your Business with Dynamic Solutions for the Digital Age',
-}
+  title: "JS | Joseph Stanion",
+  description:
+    "Empowering Your Business with Dynamic Solutions for the Digital Age",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex min-h-screen py-10">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
