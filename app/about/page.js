82926a2c7133227a4a9dc10b5ce32183d872b243
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-02 17:39:48 -0500

    added fade-in effect and background gradient on profile img

--- app/about/page.js
+++ app/about/page.js
@@ -4,20 +4,23 @@ import Styles from "./styles.module.css";
 export default function About() {
   return (
     <div className="flex justify-evenly w-full">
-      <div className="flex justify-center h-fit w-2/5 border-y border-mdGray p-4">
+      <div className="flex justify-center h-fit w-2/5 border-y border-blue p-4">
       <span className="text-yellow text-8xl">J</span>
       <p className="text-ltGray text-6xl">oseph 
       <br />
       <span className="text-yellow text-8xl">S</span>tanion</p>
 
       </div>
+      <div className="">
       <Image
-      className={`${Styles.blurImage} rounded-full h-fit self-center`}
+      id={Styles.profile}
+      className={`${Styles.fadeIn} bg-gradient-to-l from-blue h-fit`}
       width={500}
       height={300}
-      src="/profile.jpg"
+      src="/profile-3.jpg"
       alt="Picture of the author"
       />
+      </div>
     </div>
   );
 }
