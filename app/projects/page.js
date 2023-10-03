637c39b3c157988f001f2ebdbc78ce472a24c3ba
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-02 21:23:24 -0500

    added title and buttons for project sorting

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,3 +1,17 @@
 export default function Project() {
-  return <h1>This is my project page!</h1>
+  return (
+    <>
+      <div className="flex flex-col w-full items-center">
+        <h1 className="border-y border-ltGray text-4xl text-ltGray py-5">
+          PROJECTS
+        </h1>
+        <div className="flex justify-evenly items-start text-ltGray text-xl w-full pt-10">
+          <button className="hover:text-blue">ALL</button>
+          <button className="hover:text-blue">JAVASCRIPT</button>
+          <button className="hover:text-blue">REACT JS</button>
+          <button className="hover:text-blue">NEXT JS</button>
+        </div>
+      </div>
+    </>
+  );
 }
