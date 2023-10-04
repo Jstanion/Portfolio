474e506ed401a7831c429806a77c231a0c7b2183
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-03 19:01:32 -0500

    added screenshots to project page

--- app/projects/page.js
+++ app/projects/page.js
@@ -13,13 +13,58 @@ export default function Project() {
           <button className="hover:text-blue">REACT JS</button>
           <button className="hover:text-blue">NEXT JS</button>
         </div>
-        <div className="flex jusitfy-center items-center h-full">
-          <Image
-            src="/coding-quiz.png"
-            width={300}
-            height={300}
-            alt="Coding Quiz"
-          />
+        <div className="flex flex-wrap grid grid-cols-3 gap-x-20 gap-y-2 jusitfy-center items-center h-full w-full">
+          
+
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/thisLittlePiggy.png"
+              width={300}
+              height={300}
+              alt="This Little Piggy app thumbnail"
+            />
+          </div>
+
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/mrMovie.png"
+              width={300}
+              height={300}
+              alt="Mr Movie app thumbnail"
+            />
+          </div>
+
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/weatherForecast.png"
+              width={300}
+              height={300}
+              alt="Weather Forecast app thumbnail"
+            />
+          </div>
+
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/taskScheduler.png"
+              width={300}
+              height={300}
+              alt="Task Scheduler app thumbnail"
+            />
+          </div>
+
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/passwordGenerator.png"
+              width={300}
+              height={300}
+              alt="Password Generator app thumbnail"
+            />
+          </div>
         </div>
       </div>
     </>
