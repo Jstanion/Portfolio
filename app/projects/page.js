b4500019314ef7a55980dba66bdf8a0de6af64be
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-03 19:53:17 -0500

    added grid and text properties to projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,85 +1,62 @@
 import Image from "next/image";
-
+import { FaDesktop, FaGithub } from "react-icons/fa";
 
 export default function Project() {
   return (
     <>
-      <div className="flex flex-col w-full items-center">
+      <div className="flex flex-col w-full items-center justify-evenly">
         <h1 className="border-y border-ltGray text-4xl text-ltGray py-5">
           PROJECTS
         </h1>
         <div className="flex justify-evenly items-start text-ltGray text-xl w-full pt-10">
-          <button className="hover:text-blue">ALL</button>
-          <button className="hover:text-blue">JAVASCRIPT</button>
-          <button className="hover:text-blue">REACT JS</button>
-          <button className="hover:text-blue">NEXT JS</button>
+          <button className="hover:bg-blue py-1 px-6">ALL</button>
+          <button className="hover:bg-blue py-1 px-6">JAVASCRIPT</button>
+          <button className="hover:bg-blue py-1 px-6">REACT JS</button>
+          <button className="hover:bg-blue py-1 px-6">NEXT JS</button>
         </div>
-        <div className="flex flex-wrap grid grid-cols-3 gap-x-20 gap-y-2 jusitfy-center items-center h-full w-full">
-          <div className="flex flex-col justify-end items-end rounded-lg bg-dkGray ">
-            <p className="text-center w-full">short_Stack</p>
-            <div className="flex">
-              <div className="flex flex-col justify-center">
-
-              </div>
-              <Image
-                className="rounded-lg"
-                src="/shortStack.png"
-                width={300}
-                height={300}
-                alt="Short Stack Videos app thumbnail"
-              />
-            </div>
-          </div>
-
-          <div>
-            <Image
-              className="rounded-lg"
-              src="/thisLittlePiggy.png"
-              width={300}
-              height={300}
-              alt="This Little Piggy app thumbnail"
-            />
-          </div>
-
-          <div>
-            <Image
-              className="rounded-lg"
-              src="/mrMovie.png"
-              width={300}
-              height={300}
-              alt="Mr Movie app thumbnail"
-            />
-          </div>
-
-          <div>
-            <Image
-              className="rounded-lg"
-              src="/weatherForecast.png"
-              width={300}
-              height={300}
-              alt="Weather Forecast app thumbnail"
-            />
-          </div>
-
-          <div>
-            <Image
-              className="rounded-lg"
-              src="/taskScheduler.png"
-              width={300}
-              height={300}
-              alt="Task Scheduler app thumbnail"
-            />
-          </div>
-
-          <div>
-            <Image
-              className="rounded-lg"
-              src="/passwordGenerator.png"
-              width={300}
-              height={300}
-              alt="Password Generator app thumbnail"
-            />
-          </div>
+        <div className="flex flex-wrap grid grid-cols-3 bg-white">
+          <Image
+            className=""
+            src="/shortStack.png"
+            width={350}
+            height={350}
+            alt="Short Stack Videos app thumbnail"
+          />
+          <Image
+            className=""
+            src="/thisLittlePiggy.png"
+            width={350}
+            height={350}
+            alt="This Little Piggy app thumbnail"
+          />
+          <Image
+            className=""
+            src="/mrMovie.png"
+            width={350}
+            height={350}
+            alt="Mr Movie app thumbnail"
+          />
+          <Image
+            className=""
+            src="/weatherForecast.png"
+            width={350}
+            height={350}
+            alt="Weather Forecast app thumbnail"
+          />
+          <Image
+            className=""
+            src="/taskScheduler.png"
+            width={350}
+            height={350}
+            alt="Task Scheduler app thumbnail"
+          />
+          <Image
+            className=""
+            src="/passwordGenerator.png"
+            width={350}
+            height={350}
+            alt="Password Generator app thumbnail"
+          />
         </div>
       </div>
     </>
