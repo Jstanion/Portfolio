1e6a4cc481118f35a61bdc612c31d854593ffc52
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-09 11:48:41 -0500

    added front-end skills icons

--- app/skills/page.js
+++ app/skills/page.js
@@ -1,4 +1,7 @@
-import { TbBrandJavascript } from "react-icons/tb";
+import { DiJavascript } from "react-icons/di";
+import { FaCss3Alt, FaHtml5, FaReact, FaBootstrap } from "react-icons/fa";
+import { SiTailwindcss, SiBulma } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
 export default function Project() {
   return (
@@ -8,10 +11,38 @@ export default function Project() {
           <span className="pl-10">SKILLS</span>
         </h1>
       </div>
-      <div className="flex flex-wrap justify-center bg-gradient-to-r from-blue border-l-8 border-yellow rounded-l-full h-2/3 w-full">
-        <div>
-          <TbBrandJavascript className="text-yellow text-8xl" />
+      <div className="flex flex-wrap justify-evenly items-center bg-gradient-to-r from-blue border-l-8 border-yellow rounded-l-full h-1/3 w-full">
+        <div className="flex flex-col items-center">
+          <FaHtml5 className="text-orange text-8xl" />
+          <p>HTML5</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <FaCss3Alt className="text-yellow text-8xl" />
+          <p>CSS3</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <DiJavascript className="text-yellow text-8xl" />
           <p>JavaScript</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <SiTailwindcss className="text-orange text-8xl" />
+          <p>Tailwind</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <FaBootstrap className="text-orange text-8xl" />
+          <p>Bootstrap</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <SiBulma className="text-orange text-8xl" />
+          <p>Bulma</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <FaReact className="text-orange text-8xl" />
+          <p>React</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <TbBrandNextjs className="text-orange text-8xl" />
+          <p>Next JS</p>
         </div>
       </div>
     </div>
