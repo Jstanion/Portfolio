2125c64e3addfeb47149f90de427399c0b7d82ae
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-10 18:36:02 -0500

    created title section

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,13 @@
 export default function Project() {
-  return <h1>This is my contact page!</h1>
+  return (
+    <div className="flex flex-col justify-between items-end w-full">
+      <div className="flex justify-end w-full">
+        <div className="flex justify-end items-center w-full">
+          <h1 className="border-l-4 rounded-l-full border-y border-yellow bg-gradient-to-r from-blue text-4xl text-ltGray py-4 w-9/12 shadow-lg shadow-black">
+            <span className="pl-10">CONTACT</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
