2aac3c05f9da610c6f829001e726b9e2572058e6
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-16 02:21:15 -0500

    created hero page and added content

---
+++ app/hero/page.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="flex w-full text-center items-center justify-center">
+      <p className="text-4xl text-ltGray">
+        Hello, I&apos;m{" "}
+        <span className="text-yellow dark:drop-shadow-[0_0_0.3rem_#ffffff70] animate-pulse">
+          Joseph Stanion.
+        </span>
+        <br />
+        I&apos;m a{" "}
+        <span className="text-dkGray dark:drop-shadow-[0_0_0.3rem_#ffffff70] animate-pulse">
+          Full-Stack Web Developer.
+        </span>
+      </p>
+    </div>
+  );
+}
