9643c5152054eeff37e6502db40df37e3ee9f2f2
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-10-16 12:30:19 -0500

    fixed background gradient color

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,10 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col justify-between min-h-screen">
+        <div className="flex flex-col justify-between h-screen">
           <div className="flex h-full">
             <Navbar />
-            {children}
+            <div className="flex justify-center items-center w-full">
+              {children}
+            </div>
           </div>
           <Footer />
         </div>
