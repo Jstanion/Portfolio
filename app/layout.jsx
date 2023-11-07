2785da79a4af2057d210c9e3f4bedb1ad2d5d175
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-11-07 12:43:30 -0600

    moved component folder into app directory

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
