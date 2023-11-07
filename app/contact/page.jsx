03a05beacaacdd023000e635f534afdbc9217bc7
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-11-07 13:15:23 -0600

    resolved merge conflict issues - footer still missing

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactForm from '@/components/contactForm/ContactForm';
+import ContactForm from '@/app/components/contactForm/ContactForm';
 
 const Contact = () => {
   return (
