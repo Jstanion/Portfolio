dcc979305f2a8518e1edb4f555513bdb6e1b58ed
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-11-07 12:47:08 -0600

    moved Hero component content into home page route for clean up

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
-  <main>
-    <Hero />
+  <main className="flex w-full text-center items-center justify-center">
+      <p className="text-4xl text-mdGray">
+        Hello, I&apos;m{" "}
+        <span className="text-yellow dark:drop-shadow-[0_0_0.3rem_#e8c54770] animate-pulse">
+          Joseph Stanion.
+        </span>
+        <br />
+        I&apos;m a{" "}
+        <span className="text-blue dark:drop-shadow-[0_0_0.3rem_#5c80bc70] animate-pulse">
+          Full-Stack Web Developer.
+        </span>
+      </p>
   </main>
   )
 }
