4edf561c8b7bd4929a053d6cb309270f515df68f
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-11-08 21:38:35 -0600

    imported styles module and added CSS properties for animation

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ContactForm from "../components/contactForm/ContactForm";
+import Styles from "../contact/styles.module.css";
 
 const Contact = () => {
   return (
     <div className="flex flex-col items-end w-full h-full">
       <div className="flex justify-end w-full">
-        <div className="flex justify-end w-full">
+        <div className={`${Styles.titleDiv} flex justify-end w-full`}>
           <h1 className="border-l-4 rounded-l-full border-y border-yellow bg-gradient-to-r from-blue text-4xl text-ltGray font-extralight py-4 w-9/12 shadow-lg shadow-black">
             <span className="pl-10 text-yellow">CONTACT</span>
             <span className="text-xs"> </span>
@@ -14,7 +15,7 @@ const Contact = () => {
         </div>
       </div>
       <div className="flex justify-center items-center h-full">
-        <section className="flex flex-col justify-center items-center my-4 w-4/5">
+        <section className={`${Styles.fadeIn} flex flex-col justify-center items-center my-4 w-4/5`}>
           <p className="my-4 text-sm">
             Thank you for your interest. Thank you for your interest. My
             commitment is to deliver innovative and tailored solutions to meet
